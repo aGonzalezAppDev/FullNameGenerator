5a56e5cd48980cd27df985111a8dbcc94e5c2584
aGonzalezAppDev <agonzalez@appdev.yearup.org>
2023-04-17 11:20:41 -0400

    Second commit

--- src/main/java/com/learntocode/FullNameGenerator.java
+++ src/main/java/com/learntocode/FullNameGenerator.java
@@ -9,15 +9,23 @@ public class FullNameGenerator {
         // prompt user for name
         System.out.println("Please enter your name");
         System.out.println("First Name: ");
-        String firstName = myScanner.nextLine();
+        String firstName = myScanner.nextLine().trim();
         System.out.println("Middle Name: ");
-        String middleName = myScanner.nextLine();
+        String middleName = myScanner.nextLine().trim();
+        // print middle name trimmed with dot
+        if (!middleName .isEmpty()) {
+            System.out.println(middleName.charAt(0) + ".");
+        }
+
         System.out.println("Last Name: ");
-        String lastName = myScanner.nextLine();
+        String lastName = myScanner.nextLine().trim();
+
         System.out.println("Suffix: ");
+        String suffix = myScanner.nextLine().trim();
         // print comma before suffix if there is one
-        
-        String suffix = myScanner.nextLine();
+        if (!suffix.isEmpty()) {
+            suffix += ",";
+        }
 
         // create array
         String [] myList = {firstName, middleName, lastName, suffix};
